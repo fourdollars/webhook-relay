d101531bfc323011f83327be9ff3cda4d2da8f89
Shih-Yuan Lee (FourDollars) <fourdollars@debian.org>
2023-05-23 12:41:43 +0800

    Add the Basic Auth for the relay list.

--- index.js
+++ index.js
@@ -46,7 +46,24 @@ app.get('/stream', (req, res) => {
     sse.init(req, res)
 })
 
-app.get('/relay/', (req, res) => {
+function isAuth(req, res, next) {
+    const auth = {
+        username: 'username',
+        password: 'password'
+    }
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+    if (username && password && username === auth.username && password === auth.password) {
+      next()
+    } else {
+      if (!req.headers.authorization) {
+          res.set('WWW-Authenticate', 'Basic realm="401"')
+      }
+      res.status(401).send('Authentication required')
+    }
+}
+
+app.get('/relay/', isAuth, (req, res) => {
     let output = {}
     for (let key in pool) {
         output[key] = pool[key].counter
